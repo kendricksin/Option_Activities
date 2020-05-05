00fb6c80f492eebe68188f717795b33aa55ec2e9
unikend <39403022+kendricksin@users.noreply.github.com>
2020-05-05 12:11:31 +0800

    Add files via upload

--- unusual_activity.py
+++ unusual_activity.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import datetime
 
 # Enter the file name here
-df = pd.read_csv('unusual_options_30apr.csv')
+df = pd.read_csv('unusual_options_4may.csv')
 
 # Choose the Ticker
 Ticker = 'SPY'
@@ -13,21 +13,37 @@ Ticker = 'SPY'
 df['Price'] = df.Volume * df.Midpoint
 df = df.sort_values('Price', axis=0, ascending=False)
 df['Exp Date'] = pd.to_datetime(df['Exp Date'])
+# purchase_time = []
+# for timing in df['Time']:
+#     purchase_time.append(datetime.time(int(timing[0:2]), int(timing[3:5])))
+# df['Time'] = purchase_time
 df_puts = df.loc[(df['Symbol'] == Ticker) & (df['Type'] == 'Put')]
 df_calls = df.loc[(df['Symbol'] == Ticker) & (df['Type'] == 'Call')]
 
-# This is the plot for the bet
-plt.scatter(df_puts.Strike, df_puts['Exp Date'],
-            s=df_puts['Price']/max(df_puts['Price'])*2000,
-            alpha=0.5, c='red')
-plt.scatter(df_calls.Strike, df_calls['Exp Date'],
-            s=df_calls['Price']/max(df_puts['Price'])*2000,
-            alpha=0.5, c='green')
+# Add time or date filters
+# df_puts = df_puts.loc[(df_puts['Time'] < datetime.time(15, 30))]
+# df_calls = df_calls.loc[(df_calls['Time'] < datetime.time(15, 30))]
 
-# Adjust the limits
-plt.xlim(265, 315)
-plt.ylim(datetime.date(2020,4,15), datetime.date(2020,6,30))
-# plt.xlabel('Strike Price')
-# plt.ylabel('Strike Date')
+# This is the plot
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+# Adjust the sizes color and transparency of the plot
+transparency, sizing = 0.5, 2000
+largest_bet = max(df.loc[(df['Symbol'] == Ticker), 'Price'])
+call_color = 'green'
+put_color = 'red'
+
+plt.scatter(df_puts.Strike, df_puts['Exp Date'],
+            s=df_puts['Price']/largest_bet*sizing,
+            alpha=transparency, c=put_color)
+plt.scatter(df_calls.Strike, df_calls['Exp Date'],
+            s=df_calls['Price']/largest_bet*sizing,
+            alpha=transparency, c=call_color)
+
+# Adjust the date and range of the plot
+plt.xlim(265, 300)
+plt.ylim(datetime.date(2020,5,1), datetime.date(2020,5,29))
+ax.set_xticks(np.arange(265, 300, 5))
 plt.grid(color='grey', linestyle='-', linewidth=0.2)
 
